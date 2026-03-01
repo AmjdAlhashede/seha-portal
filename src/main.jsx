43bc9a5bd9e321c34c2148d119f7db6151070158
Amjd Alhashede <amjdalhashede@gmail.com>
2026-03-01 19:48:48 +0300

    feat: Initialize React application entry point with Ant Design configuration and routing.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import SehaRoutes from './routes/SehaRoutes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './i18n/i18n';
 
